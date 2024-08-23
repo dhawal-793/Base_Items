13a567945d57917edfda956b66dbb76b50b50e1c
Arun2089 <arunlohar@gkmit.co>
2024-08-23 16:50:37 +0530

    Update docker.sh

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -17,4 +17,5 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 
 # Install Compose
 sudo apt-get update
-sudo apt-get install docker-compose-plugin
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
