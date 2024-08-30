d37445429ec0f381e88c7eff20a39554c4a7d53b
Arun2089 <arunlohar@gkmit.co>
2024-08-30 10:24:17 +0530

    docker sock permissions updated

--- Scripts/sh/docker.sh
+++ Scripts/sh/docker.sh
@@ -19,3 +19,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 sudo apt-get update
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# docker sock permissions updated
+chmod 777 /var/run/docker.sock
