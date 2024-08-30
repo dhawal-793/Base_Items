549b1ea06d7725798aba6910ad95309c5c673542
Arun Lohar <arunlohar@gkmit.co>
2024-08-30 10:21:46 +0530

    Folder structure changed

--- Scripts/main.sh
+++ Scripts/main.sh
@@ -1,7 +1,16 @@
-# sh ./ansible.sh
-# sh ./docker.sh
-# sh ./terraform.sh
-# sh ./awscli.sh
-# sh ./jenkins.sh
+# install packages
+
+
+# sh ./sh/docker.sh
+# sh ./sh/ansible.sh
+# sh ./sh/terraform.sh
+# sh ./sh/awscli.sh
+# sh ./sh/jenkins.sh
+
+
+# User add to Groups
+
+# sh ./sh/jenkins-user-to-docker-group.sh
+# sh ./sh/ubuntu-user-to-docker-group.sh
 
 
